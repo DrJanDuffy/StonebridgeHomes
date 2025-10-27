7bb5c83d2068e5eca8e9470514ed98042d5e28a9
genekellyboyle <DrDuffy@bhhsnv.com>
2025-10-27 10:41:23 -0700

    Fix SSR entry file for Vercel deployment

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -1,10 +1,8 @@
 import { renderToStream, type RenderToStreamOptions } from '@builder.io/qwik/server';
-import { manifest } from '@qwik-city-plan';
-import { Root } from './root';
+import Root from './root';
 
-export function render(opts: RenderToStreamOptions) {
+export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
-    manifest,
     ...opts,
   });
 }
